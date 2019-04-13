300c2e0d0fb9f80939484ef27e92ad7bbcaa8636
bckumar2080 <40674604+bckumar2080@users.noreply.github.com>
2019-04-13 02:47:45 -0400

    Update activity.go

--- activity.go
+++ activity.go
@@ -2,29 +2,30 @@
 package token
 
 import (
-	b64 "encoding/base64"
+	// b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
-
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 const (
-	grantType      = "password"
+	ivgrantType    = "grantType"
 	ivUsername     = "username"
 	ivPassword     = "password"
-	ivScope        = "scope"
-	ivBasicAuth    = "basicauth"
-	ivUrl          = "accessURL"
+	ivClientId     = "clientid"
+	ivClientSecret = "clientsecret"
+	ivtokenUrl     = "tokenURL"
 	ovToken        = "accesstoken"
 	ovTokenType    = "tokentype"
-	ovExpires      = "expiresin"
-	ovRefreshToken = "refreshtoken"
-	ovScope        = "scope"
+	ovInstance_Url = "instance_url"
+	ovId           = "id"
+	ovIssued_at    = "issued_at"
+	ovSignature    = "signature"
 )
 
 // log is the default package logger
@@ -49,23 +50,36 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	// Get the user provided data
+	tokenURL := context.GetInput(ivtokenUrl).(string)
 	username := context.GetInput(ivUsername).(string)
 	password := context.GetInput(ivPassword).(string)
-	scope := context.GetInput(ivScope).(string)
-	url := context.GetInput(ivUrl).(string)
-    auth := context.GetInput(ivBasicAuth).(string)
-	encodedAuth := b64.StdEncoding.EncodeToString([]byte(auth))
+	clientid := context.GetInput(ivClientId).(string)
+	clientsecret := context.GetInput(ivClientSecret).(string)
+	grantType := context.GetInput(ivgrantType).(string)
+	// auth := context.GetInput(ivBasicAuth).(string)
+	// encodedAuth := b64.StdEncoding.EncodeToString([]byte(auth))
+
+	queryParam := url.Values{
+	  //"tokenURL":    {tokenURL},
+		"grant_type":    {grantType},
+		"client_id":     {clientid},
+		"client_secret": {clientsecret},
+		"username":      {username},
+		"password":      {password},
+	}
+
+	body := strings.NewReader(queryParam.Encode())
 
 	// Get the token from TIBCO Cloud Mashery
-	payload := strings.NewReader(fmt.Sprintf("grant_type=%s&username=%s&password=%s&scope=%s", grantType, username, password, scope))
+	//payload := strings.NewReader(fmt.Sprintf("grant_type=%s&username=%s&password=%s&clientid=%s&clientsecret=%s", grantType, username, password, clientid, clientsecret))
 
-	req, err := http.NewRequest("POST", url, payload)
+	req, err := http.NewRequest("POST", tokenURL, body)
 	if err != nil {
 		return false, err
 	}
 
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
-	req.Header.Add("authorization", fmt.Sprintf("Basic %s", encodedAuth))
+	// req.Header.Add("authorization", fmt.Sprintf("Basic %s", encodedAuth))
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -74,21 +88,23 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	resPonsebody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return false, err
 	}
 
+	fmt.Println(string(resPonsebody))
 	// Set the output value in the context
 	var data map[string]interface{}
-	if err := json.Unmarshal(body, &data); err != nil {
+	if err := json.Unmarshal(resPonsebody, &data); err != nil {
 		return false, err
 	}
-	context.SetOutput(ovExpires, data["expires_in"])
-	context.SetOutput(ovRefreshToken, data["refresh_token"])
-	context.SetOutput(ovScope, data["scope"])
+	context.SetOutput(ovInstance_Url, data["instance_url"])
+	context.SetOutput(ovId, data["id"])
+	context.SetOutput(ovIssued_at, data["issued_at"])
 	context.SetOutput(ovToken, data["access_token"])
 	context.SetOutput(ovTokenType, data["token_type"])
+	context.SetOutput(ovSignature, data["signature"])
 
 	return true, nil
 }
